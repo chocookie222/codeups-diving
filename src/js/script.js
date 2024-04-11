73b43dcc5254ed26ce404251b8c9115d75c281ea
Aya <165669980+chocookie222@users.noreply.github.com>
2024-04-11 21:08:52 +0900

    MVとheaderピクパ

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,73 @@
 jQuery(function ($) {
-  // この中であればWordpressでも「$」が使用可能になる
+  //ナビバートグル
+  $(".js-hamburger").on("click", function () {
+    if ($(".js-hamburger").hasClass("is-open")) {
+      $(".js-drawer-menu").fadeOut();
+      $(this).removeClass("is-open");
+    } else {
+      $(".js-drawer-menu").fadeIn();
+      $(this).addClass("is-open");
+    }
+  });
 });
 
-// スライダー
-$(".slider").slick({
-  autoplay: true, //自動的に動き出すか。初期値はfalse。
-  infinite: true, //スライドをループさせるかどうか。初期値はtrue。
-  slidesToShow: 3, //スライドを画面に3枚見せる
-  slidesToScroll: 3, //1回のスクロールで3枚の写真を移動して見せる
-  prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
-  nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
-  dots: false, //下部ドットナビゲーションの表示
-  responsive: [
-    {
-      breakpoint: 769, //モニターの横幅が769px以下の見せ方
-      settings: {
-        slidesToShow: 1, //スライドを画面に2枚見せる
-        slidesToScroll: 1, //1回のスクロールで2枚の写真を移動して見せる
-      },
-    },
-  ],
+//header 背景
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(".mv").height() < $(this).scrollTop()) {
+      $(".js-header").addClass("change-color");
+    } else {
+      $(".js-header").removeClass("change-color");
+    }
+  });
+});
+
+// スライダー(swiper)
+// const swiper = new Swiper(".swiper", {
+//   loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  speed: 400,
+  autoplay: {
+    delay: 1000, // 1秒後に次のスライド
+    disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// 画像出現アニメーション
+var box = $(".colorbox"),
+  speed = 700;
+
+box.each(function () {
+  $(this).append('<div class="color"></div>');
+  var color = $(this).find($(".color")),
+    image = $(this).find("img");
+  var counter = 0;
+  image.css("opacity", "0");
+  color.css("width", "0");
+  color.on("inview", function () {
+    if (counter == 0) {
+      $(this)
+        .delay(200)
+        .animate({ width: "100%" }, speed, function () {
+          image.css("opacity", "1");
+          $(this).css({ left: "0", right: "auto" });
+          $(this).animate({ width: "0%" }, speed);
+        });
+      counter = 1;
+    }
+  });
 });
