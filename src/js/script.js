80055ac392a8c89890af49755d594180a7135f73
chocookie222 <chocookie2222@gmail.com>
2024-04-29 11:50:47 +0900

    css zoom後修正

--- src/js/script.js
+++ src/js/script.js
@@ -35,15 +35,20 @@ jQuery(function ($) {
     });
   });
 
-  // スライダー(mv)
-  var swiper = new Swiper(".mvSwiper", {
-    loop: true,
-    speed: 400,
-    autoplay: {
-      delay: 3000, // 1秒後に次のスライド
-      disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
-    },
-    allowTouchMove: false,
+  // フェード(mv)
+  $(".slider").slick({
+    fade: true, //切り替えをフェードで行う。初期値はfalse。
+    autoplay: true, //自動的に動き出すか。初期値はfalse。
+    autoplaySpeed: 3000, //次のスライドに切り替わる待ち時間
+    speed: 2000, //スライドの動きのスピード。初期値は300。
+    infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 1, //スライドを画面に3枚見せる
+    slidesToScroll: 1, //1回のスクロールで3枚の写真を移動して見せる
+    arrows: false, //左右の矢印あり
+    dots: false, //下部ドットナビゲーションの表示
+    pauseOnFocus: false, //フォーカスで一時停止を無効
+    pauseOnHover: false, //マウスホバーで一時停止を無効
+    pauseOnDotsHover: false, //ドットナビゲーションをマウスホバーで一時停止を無効
   });
 
   // スライダー(キャンペーン)
